affe552d3a127dec247ffa7ce656573c7858e8be
DESKTOP-PM0ONBD\danie <dannymunn1995@gmail.com>
2018-08-27 22:53:38 +0100

    WIP

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,10 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-tailwind');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .tailwind();
 
 mix.copy('node_modules/animate.css/animate.css', 'public/css/vendor.css');
 
