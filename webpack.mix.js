dcda914539202fee43d86c7084ced3c5ca06b324
daniel munn <dannymunn1995@gmail.com>
2018-09-19 15:13:45 +0100

    Purgecss changes and removal of unneeded imports

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,20 +2,18 @@
 /* Imports */
 
 let mix = require('laravel-mix');
-let tailwindcss = require("tailwindcss");
-let glob = require("glob-all");
-let PurgecssPlugin = require("purgecss-webpack-plugin");
+// let tailwindcss = require("tailwindcss");
+// let glob = require("glob-all");
+// let PurgecssPlugin = require("purgecss-webpack-plugin");
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
 /* Compilation configurations */
 
-mix.options({
-    postCss: [tailwindcss("./tailwind.js")]
-});
-
-require('laravel-mix-tailwind');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .tailwind();
+   .tailwind()
+   .purgeCss();
 
 mix.copy('node_modules/animate.css/animate.css', 'public/css/vendor.css');
 
@@ -23,32 +21,20 @@ mix.browserSync('portfolio.test:8000');
 
 /* CSS Purging */
 
-class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-    }
-}
+// class TailwindExtractor {
+//     static extract(content) {
+//         return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+//     }
+// }
+
+// mix.postCss('resources/assets/sass/app.scss', 'public/css', [
+//     tailwindcss('./tailwind.js')
+// ]);
+
+// mix.options({
+//     postCss: [tailwindcss("./tailwind.js")]
+// });
 
 if (mix.inProduction()) {
-
     mix.version();
-
-    mix.webpackConfig({
-        plugins: [
-            new PurgecssPlugin({
-
-                paths: glob.sync([
-                    path.join(__dirname, "resources/views/**/*.blade.php"),
-                    path.join(__dirname, "resources/assets/js/**/*.vue")
-                ]),
-                extractors: [
-                    {
-                        extractor: TailwindExtractor,
-
-                        extensions: ["html", "js", "php", "vue"]
-                    }
-                ]
-            })
-        ]
-    });
 }
